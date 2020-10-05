6afdd1abaf9ad193656cd1f6345dbad33309b5f2
Wade-Coplen <wadecoplen@gmail.com>
2020-10-04 19:54:58 -0700

    working on searching.py. Still isnt working, getting -1 != 1.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,27 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # Base Case (where we will stop)
+    if start >= end:
+    
+        mid = (start + end) // 2
+        # if the element is the middle
+        if arr[mid] == target:
+            return mid
+        # if the element is < mid it will be on the left side array
+        elif arr[mid] > target:
+            # else the elemnt is in >, will be on the right side
+            return binary_search(arr, end, mid -1, target)
+        else:
+            return binary_search(arr, mid + 1, start, target)
 
+           
+            
+
+    
+  
+
+        
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -9,6 +29,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+#def agnostic_binary_search(arr, target):
     # Your code here
 
